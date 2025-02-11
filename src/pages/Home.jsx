6a499d3a36b6584954aec87f9e038d07dfd3c14a
Masson Corlette <massoncorlette07@gmail.com>
2025-02-11 14:25:19 -0500

    feat: setting up fetch logic for top 100 games per year, 40 games per request seems to be limit. page_count property loads in next score rated games

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import DefaultHomePage from "./DefaultHomePage";
 import PageOne from "./PageOne";
 import PageTwo from "./PageTwo";
+import queryForTop100GameData from "../api.js"
 import '../styles/App.css'
 
 function Home() {
@@ -12,6 +13,9 @@ function Home() {
     <div>
     <h1>Hello from home page!</h1>
     <p>So, how are you?</p>
+    <div>
+      {console.log(queryForTop100GameData(1998))};
+    </div>
     <hr />
     <h2>The page visited is here:</h2>
     {name === "pageone" ? (
