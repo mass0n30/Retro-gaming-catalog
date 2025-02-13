96e076f78aa9cf89f8052cdcc2c3796bdc4a41ca
massoncorlette <massoncorlette07@gmail.com>
2025-02-13 01:19:00 -0500

    feat: setting inital fetch for default home page fetch of top 100 games from 85', Home parent component, HomeBody child component rendering grandchild components dynamically

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,44 @@
 {/* import { useState, useEffect } from 'react' */}
 import { useParams } from "react-router-dom";
-import DefaultHomePage from "./DefaultHomePage";
-import PageOne from "./PageOne";
-import PageTwo from "./PageTwo";
-import queryForTop100GameData from "../api.js"
+import {useState, useEffect} from 'react';
+
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+import queryForTop100GameData from "../api.js";
+import GameCardBody from "./HomeBody";
+
 import '../styles/App.css'
 
 function Home() {
-  const { name } = useParams();
+  const [games, setGames] = useState[null];
+
+  const [year, setYear] = useState[1985];
+
+  useEffect(() => {
+    let ignore = false;
+    queryForTop100GameData(year).then(data => {
+      if(!(ignore)) {
+        setGames(data);
+      }
+    });
+    return () => {
+      console.log("Cleanup");
+      ignore = true;
+    };
+  }, [year]);
 
   return (
     <div>
     <h1>Hello from home page!</h1>
     <p>So, how are you?</p>
     <div>
-      {console.log(queryForTop100GameData(1998))};
+      <GameCardBody games={games}/>
     </div>
     <hr />
-    <h2>The page visited is here:</h2>
-    {name === "pageone" ? (
-      <PageOne />
-    ) : name === "pagetwo" ? (
-      <PageTwo />
-    ) : (
-      <DefaultHomePage />
-    )}
+    <div id='paginationContainer'>
+      <Pagination count={10} shape="rounded" />
+    </div>
   </div>
   )
 }
---
+++ src/pages/HomeBody.jsx
@@ -0,0 +1,13 @@
+
+{/* import { useState, useEffect } from 'react' */}
+{/*maybe import local styles */}
+
+
+function GameCardBody(games) {
+
+  console.log(games);
+
+}
+
+
+export default {GameCardBody};
