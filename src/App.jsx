193625fac867b891cba43fc4b3230b7fd394a777
Masson Corlette <massoncorlette07@gmail.com>
2025-02-17 21:46:20 -0500

    refactor: deciding to stick with just using useEffect fetching instead of loaders as recommended to keep things more straightforward.Passing data between routes and components seems to serve as difficult using loaders, perhaps large cached data is where loaders shine.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
-  const [games, setGames] = useState();
+  const [games, setGames] = useState([]);
 
   //setState data for route change kept here
+
+  const handleSetGames = (newGames) => {
+
+    setGames(newGames);
+  }
+
 
 
   return (
     <div>
       <Navbar />
       <main>
-        <Outlet context={[games,setGames]}/>
+        <Outlet context={{games,handleSetGames}}/>
       </main>
       <Footer />
     </div>
