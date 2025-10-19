f4400aceb2024a1343f20d724290717b662b6666
massoncorlette <massoncorlette07@gmail.com>
2025-10-19 02:41:48 -0400

    feat: setting up my useEffect hooks for selected categories, looking to refactor routes on backend next commit, refactoring out some twitch fetch logic since my db is populated

--- backend/controllers/viewController.js
+++ backend/controllers/viewController.js
@@ -4,18 +4,20 @@ const {prisma} = require("../db/prismaClient.js");
 
 async function handleGetGames(req, res, next) {
 
+  const limit = req.query.limit;
+  const offset = limit * req.query.offset;
+
   try {
 
     const games = await prisma.game.findMany({
-      take: 100
+      skip: offset,
+      take: parseInt(limit)
     });
     return games;
     
   } catch (error) {
     next(error)
   }
-
-
-}
+};
 
 module.exports = {handleGetGames};
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,19 +15,58 @@ function HomePage() {
 
     // InfiniteScroll state var
     const [hasMore, setHasMore] = useState(true);
-    const [index, setIndex] = useState(2);
+    const [index, setIndex] = useState(1);
 
-    useEffect(() => {
+    const [platform, setPlatform] = (null);
+    const [genre, setGenre] = (null);
+    const [developer, setDeveloper] = (null);
+
+
+
+    // initial mount for inital games (maybe save state scroll location?)
+  useEffect(() => {
     axios
-      .get("http://localhost:5000/home/games",{
+      .get("http://localhost:5000/home/games?offset=0&limit=100",{
       })
       .then((res) => setGames(res.data.games))
       .catch((err) => console.log(err));
   }, [token, setGames]);
 
+
+  // Genre 
+  useEffect(() => {
+    if (!genre) return;
+    axios
+      .get(`http://localhost:5000/home/games/genre/${genre}`)
+      .then((res) => setGames(res.data.games))
+      .catch((err) => console.log(err));
+
+  }, [genre, setGames]);
+
+  // Platform 
+  useEffect(() => {
+    if (!platform) return;
+    axios
+      .get(`http://localhost:5000/home/games/platform/${platform}`)
+      .then((res) => setGames(res.data.games))
+      .catch((err) => console.log(err));
+
+  }, [platform, setGames]);
+
+  // Developer 
+  useEffect(() => {
+    if (!developer) return;
+    axios
+      .get(`http://localhost:5000/home/games/developer/${developer}`)
+      .then((res) => setGames(res.data.games))
+      .catch((err) => console.log(err));
+
+  }, [developer, setGames]);
+
+
     const fetchMoreData = () => {
     axios
-      .get(`http://localhost:5000/home/games?offset=${index}0&limit=12`)
+      .get(`http://localhost:5000/home/games?offset=${index}0&limit=100`)
       .then((res) => {
         setGames((prevItems) => [...prevItems, ...res.data]);
 
