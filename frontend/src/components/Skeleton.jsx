601f4ffeca8aeb2c90b4acd43cce09e038e827c4
massoncorlette <massoncorlette07@gmail.com>
2026-02-04 17:16:19 -0500

    style: fixing some overflow issues with containers and texts for mobile

--- frontend/src/components/Skeleton.jsx
+++ frontend/src/components/Skeleton.jsx
@@ -1,5 +1,6 @@
 import { Skeleton, Stack, Box } from "@mui/material";
 import styles from '../styles/components/details.module.css';
+import { FileX } from "lucide-react";
 const skeletonBg = "#1a1e27";
 const skeletonBgTransparent = "#1a1e2777";       
 const skeletonBgAlt = "#1a2b29ad";
@@ -11,22 +12,23 @@ export function GameDetailsSkeleton() {
       <>
       <div className={styles.outercontaineralt}>
         <div className={styles.detailscontainer}>
-          <div className={styles.covercontainer}>
+          <div className={styles.covercontainer}
+          style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <Skeleton 
               variant="rounded" 
-              width={370} 
-              height={700} 
+              height={800} 
+              width={'100%'}
               animation="wave"
               sx={{ bgcolor: skeletonBg, border: '2px solid', borderColor: skeletonBgAlt, borderRadius: '8px',
-                boxShadow: `0 0 10px ${skeletonHighlight}`
+                boxShadow: `0 0 10px ${skeletonHighlight}`,
               }}
             />
           </div>
           <div className={styles.datacontainer}>
             <Skeleton 
               variant="rounded" 
-              width={780} 
-              height={1000}
+              width={'100%'} 
+              height={800}
               animation="wave"
               sx={{ bgcolor: skeletonBg, border: '2px solid', borderColor: skeletonBgAlt, borderRadius: '8px',
                 boxShadow: `0 0 10px ${skeletonHighlight}`,
